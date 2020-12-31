4d0e1765648d5538d15ee2e5f07408266e08b9fc
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-01-01 00:10:40 +0530

    rotations translation flip etc of images

---
+++ transformations.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+
+img=cv.imread('photos/cat2.jpg')
+cv.imshow('boston',img)
+
+#translation of image on the axis
+def translate(img,x,y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1],img.shape[0])
+    return cv.warpAffine(img,transMat,dimensions)
+
+# -x = left
+# y = up
+# x= right
+# -y = down
+
+# translated = translate(img,100,100)
+# cv.imshow('translated',translated)
+
+#rotating an image
+def rotate(img,angle,rotPoint=None):
+    (height,width) = img.shape[:2]
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    rotMat = cv.getRotationMatrix2D(rotPoint,angle,1.0)
+    dimensions = (width,height)
+    return cv.warpAffine(img,rotMat,dimensions)
+
+# rotated = rotate(img,45)
+# cv.imshow('rotated',rotated)
+
+#flipping an image horizontal
+flip = cv.flip(img,1)
+cv.imshow('flip horizontal',flip)
+
+flip = cv.flip(img,-1)
+cv.imshow('flip horizontal and vertical',flip)
+
+#flipping an image vertical
+flip = cv.flip(img,0)
+cv.imshow('flip vertical',flip)
+
+cv.waitKey(0)
